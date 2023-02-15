b3b9c17fcf13376dc28c2311c696865c9ef449a8
yamisora-sys <20520930@ms.uit.edu.vn>
2023-02-15 21:52:54 +0700

    push

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions} from 'react-native';
 
 import React, { useState, useEffect } from 'react';
 import * as tf from '@tensorflow/tfjs';
@@ -7,7 +7,7 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { bundleResourceIO } from '@tensorflow/tfjs-react-native';
 import { Camera } from 'expo-camera';
-
+import img1 from './img/image.jpg';
 const modelJson = require('./model/model.json');
 const modelWeights = require('./model/weights.bin');
 
@@ -27,12 +27,12 @@ export default function App() {
 
     tf.ready();
 
-    (async () => {
-      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-      if (status !== 'granted') {
-        alert('Sorry, we need camera roll permissions to make this work!');
-      }
-    })();
+    // (async () => {
+    //   const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    //   if (status !== 'granted') {
+    //     alert('Sorry, we need camera roll permissions to make this work!');
+    //   }
+    // })();
   }, []);
 
   const pickImage = async () => {
@@ -43,28 +43,30 @@ export default function App() {
       quality: 1,
     });
 
-    if (!result.cancelled) {
-      setImage(result.uri);
+    if (!result.canceled) {
+      setImage(result);
     }
 
     console.log(result);
   };
 
   const predict = async () => {
-    const imageAssetPath = image.resolveAssetSource(image);
-    const response = await fetch(imageAssetPath.uri);
-    const blob = await response.blob();
-    const imageTensor = await tf.browser.fromPixels(blob);
-    const resizedImage = tf.image.resizeBilinear(imageTensor, [224, 224]);
-    const normalizedImage = tf.div(resizedImage, 255.0);
-    const batchedImage = normalizedImage.expandDims(0);
+    const imageAssetPath = image.uri
+    console.log(imageAssetPath);
+    const blob = await fetch(imageAssetPath).then(r => r.blob());
+    const array32 = await blob.readAsBytes();
+        const imageTensor = tf.browser.fromPixels(array8, image.assets[0].height, image.assets[0].width);
+        console.log(imageTensor)
+        // const resizedImage = tf.image.resizeBilinear(imageTensor, [224, 224]);
+        // const normalizedImage = tf.div(resizedImage, 255);
+        // const batchedImage = normalizedImage.expandDims(0);
 
-    const prediction = model.predict(batchedImage);
-    const predictionArray = await prediction.data();
-    const predictionIndex = predictionArray.indexOf(Math.max(...predictionArray));
+        const prediction = model.predict(imageTensor);
+        const predictionArray = prediction.data();
+        const predictionIndex = predictionArray.indexOf(Math.max(...predictionArray));
 
-    console.log(predictionIndex);
-    alert(predictionIndex);
+        console.log(predictionIndex);
+        alert(predictionIndex);
   };
 
   return (
